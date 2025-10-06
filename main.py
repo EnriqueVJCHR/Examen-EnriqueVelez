4c2ed0827a9ab1f2b8b4b396b922c9b2d330adeb
Chrona <coyovelez@hotmail.com>
2025-10-06 08:28:10 -0600

    Se agrega la funcion de calcular area de un circulo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-
-from clases.areas import Areas 
+from clases.areas import Areas
 
 def main():
     obj = Areas()
@@ -8,8 +7,9 @@ def main():
     print("=== CÁLCULO DE ÁREAS ===")
     print("1. Triángulo")
     print("2. Rectángulo")
+    print("3. Círculo")
 
-    opcion = input("Elige una opción (1 o 2): ")
+    opcion = input("Elige una opción (1, 2 o 3): ")
 
     if opcion == "1":
         obj.leer_triangulo()
@@ -17,6 +17,9 @@ def main():
     elif opcion == "2":
         obj.leer_rectangulo()
         obj.mostrar_resultado("rectángulo")
+    elif opcion == "3":
+        obj.leer_circulo()
+        obj.mostrar_resultado("círculo")
     else:
         print("Opción no válida")
 
